cbfe9a6054e8a2e280b2f05e76bb721933cd00cf
Jake Martin <jakemartin@berkeley.edu>
2023-07-05 20:53:06 -0700

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginPage from './pages/LoginPage';
@@ -18,37 +17,37 @@ function App() {
                 </div>
             </div>
         );
-    }
+    };
 
     if (!isAuthenticated) {
         return (
             <div className="App">
                 <div className="App-header">
-                    <LoginPage />
+                    <LoginPage/>
                 </div>
             </div>
 
         );
-    } else {
-        if (!user.email_verified) {
-            return (
-                <div className="App">
-                    <div className="App-header">
-                        <EmailNotVerified />
-                    </div>
-                </div>
+    };
 
-            );
-        } else {
-            return (
-                <div className="App">
-                    <div className="App-header">
-                        <Home />
-                    </div>
+    if (!user.email_verified) {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <EmailNotVerified/>
                 </div>
-            );
-        }
-    }
+            </div>
+
+        );
+    };
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,7 @@
 import React from "react";
 import LoginButton from "../components/identity/LoginButton";
-import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginPage = () => {
-    const { loginWithRedirect } = useAuth0();
     return (
         <div>
             <p>
